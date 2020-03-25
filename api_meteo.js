401f097066792347f794b45cf219c3910c5f8a08
Bastien3 <bastien.simon.etu@univ-lemans.fr>
2020-03-25 11:41:50 +0100

    affichage des données météo

--- api_meteo.js
+++ api_meteo.js
@@ -16,24 +16,53 @@ function ajaxGet(url, callback) {
     req.send(null);
 }
 
-// Traitement des données du fichier JSON reçu depuis l'API weatherbit pour Melbourne
+// Traitement des données du fichier JSON reçu depuis l'API weatherbit pour les prévisions des 3 prochains jours pour Melbourne
 ajaxGet("https://api.weatherbit.io/v2.0/forecast/daily?&city=Melbourne&state=Victoria&key=2efb0738c35141ef9ee890c303f25215", function (reponse) {
+
     var meteo = JSON.parse(reponse);
-    // Récupération de certains résultats
-    console.log(meteo.city_name);
-    console.log(meteo.data[0].wind_gust_spd);
-    // !!! Penser à faire : afficher dans la console le nombre de requêtes restantes (si possible)
-    /*var temperature = meteo.current_observation.temp_c;
-    var humidite = meteo.current_observation.relative_humidity;
-    var imageUrl = meteo.current_observation.icon_url;
-    // Affichage des résultats
-    var conditionsElt = document.createElement("div");
-    conditionsElt.textContent = "Il fait actuellement " + temperature +
-        "°C et l'humidité est de " + humidite;
-    var imageElt = document.createElement("img");
-    imageElt.src = imageUrl;
-    var meteoElt = document.getElementById("meteo");
-    meteoElt.appendChild(conditionsElt);
-    meteoElt.appendChild(imageElt);*/
+
+	// Affichage des données dans le tableau de prévisions
+    document.getElementById("previsions").innerHTML = 
+    "<tr>" + 
+	"	<td>" + meteo.data[0].datetime + "</td>" + // Date
+	"	<td>" + meteo.data[0].min_temp + "°C</td>" + // Température minimale
+	"	<td>" + meteo.data[0].max_temp + "°C</td>" + // Température maximale
+	"	<td>" + Math.round(meteo.data[0].precip) + " mm</td>" + // Pluie
+	"	<td>" + Math.round(meteo.data[0].wind_gust_spd * 3.6) + " km/h " + meteo.data[0].wind_cdir + "</td>" + // Vent (vitesse + direction)
+	"</tr>" + 
+	"<tr>" + 
+	"	<td>" + meteo.data[1].datetime + "</td>" +
+	"	<td>" + meteo.data[1].min_temp + "°C</td>" +
+	"	<td>" + meteo.data[1].max_temp + "°C</td>" +
+	"	<td>" + Math.round(meteo.data[1].precip) + " mm</td>" +
+	"	<td>" + Math.round(meteo.data[1].wind_gust_spd * 3.6) + " km/h " + meteo.data[1].wind_cdir + "</td>" +
+	"</tr>" + 
+	"<tr>" + 
+	"	<td>" + meteo.data[2].datetime + "</td>" +
+	"	<td>" + meteo.data[2].min_temp + "°C</td>" +
+	"	<td>" + meteo.data[2].max_temp + "°C</td>" +
+	"	<td>" + Math.round(meteo.data[2].precip) + " mm</td>" +
+	"	<td>" + Math.round(meteo.data[2].wind_gust_spd * 3.6) + " km/h " + meteo.data[2].wind_cdir + "</td>" +
+	"</tr>";
 });
 
+// Traitement des données issues du temps actuel
+ajaxGet("https://api.weatherbit.io/v2.0/forecast/daily?&city=Melbourne&state=Victoria&key=2efb0738c35141ef9ee890c303f25215", function (reponse) {
+
+    var meteo = JSON.parse(reponse);
+    
+	var urlImage = "https://www.weatherbit.io/static/img/icons/" + meteo.data[0].weather.icon + ".png"; // Icône fournie par l'API
+	var altImage = meteo.data[0].weather.description; // Description
+	
+	// Modifications des attributs de la balise d'image
+	document.getElementById("temps_actuel").setAttribute("src", urlImage);
+	document.getElementById("temps_actuel").setAttribute("alt", altImage);
+	
+	// Affichage des données concernant le temps actuel
+	document.getElementById("temps").innerHTML = 
+    "<tr>" + 
+	"	<td>" + meteo.data[0].temp + "°C</td>" + // Température
+	"	<td>" + Math.round(meteo.data[0].precip) + " mm</td>" + // Pluie
+	"	<td>" + Math.round(meteo.data[0].wind_gust_spd * 3.6) + " km/h " + meteo.data[0].wind_cdir + "</td>" + // Vent (vitesse + direction)
+	"</tr>"
+});
