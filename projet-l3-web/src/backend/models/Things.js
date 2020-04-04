74a342c4ec90c7508b3afaba218720c6bb27a53d
Clément <clement_dubois.etu@univ-lemans.fr>
2020-04-04 15:25:44 +0200

    Test angular

---
+++ projet-l3-web/src/backend/models/Things.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const avis = mongoose.Schema({
+    nom: {type: String, required: true},
+    circuit: {type: String, required: true},
+    commentaire: {type: String, required: true},
+});
+
+// pour les palmares des pilotes --> classé par le nombre de win
+const meilleur = mongoose.Schema({
+    name: {type: String, required: true},
+    nbPoles: {type: Number, required: true},
+    nbWin: {type: Number, required: true},
+});
+
+module.exports = mongoose.model('Thing', avis);
+module.exports = mongoose.model('Thing', meilleur);
